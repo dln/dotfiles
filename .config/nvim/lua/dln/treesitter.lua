2b6426dde0a4fde5dbb24d6311e43cc30588aa2f
Daniel Lundin <daniel@arity.se>
2022-02-14 14:14:58 +0100

    treesitter fixes

--- .config/nvim/lua/dln/treesitter.lua
+++ .config/nvim/lua/dln/treesitter.lua
@@ -46,32 +46,32 @@ require("nvim-treesitter.configs").setup {
 				["[M"] = "@class.outer"
 			}
 		},
-		select = {
-			enable = true,
-			keymaps = {
-				-- You can use the capture groups defined in textobjects.scm
-				["af"] = "@function.outer",
-				["if"] = "@function.inner",
-				["ac"] = "@class.outer",
-				["ic"] = "@class.inner",
-				-- Or you can define your own textobjects like this
-				["iF"] = {
-					python = "(function_definition) @function",
-					cpp = "(function_definition) @function",
-					c = "(function_definition) @function",
-					java = "(method_declaration) @function"
-				}
-			}
-		},
-		swap = {
-			enable = true,
-			swap_next = {
-				["<leader>l"] = "@parameter.inner"
-			},
-			swap_previous = {
-				["<leader>h"] = "@parameter.inner"
-			}
-		}
+		-- select = {
+		-- 	enable = true,
+		-- 	keymaps = {
+		-- 		-- You can use the capture groups defined in textobjects.scm
+		-- 		["af"] = "@function.outer",
+		-- 		["if"] = "@function.inner",
+		-- 		["ac"] = "@class.outer",
+		-- 		["ic"] = "@class.inner",
+		-- 		-- Or you can define your own textobjects like this
+		-- 		["iF"] = {
+		-- 			python = "(function_definition) @function",
+		-- 			cpp = "(function_definition) @function",
+		-- 			c = "(function_definition) @function",
+		-- 			java = "(method_declaration) @function"
+		-- 		}
+		-- 	}
+		-- },
+		-- swap = {
+		-- 	enable = true,
+		-- 	swap_next = {
+		-- 		["<leader>l"] = "@parameter.inner"
+		-- 	},
+		-- 	swap_previous = {
+		-- 		["<leader>h"] = "@parameter.inner"
+		-- 	}
+		-- }
 	},
 	--- nvim-ts-autotag ---
 	autotag = {
