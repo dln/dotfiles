f7a2f68a75ee9e57f55a645b76d08d99e38c30d5
Daniel Lundin <daniel@arity.se>
2022-02-16 14:26:02 +0100

    nvim: more colors

--- .config/nvim/lua/plugins/zenbones.lua
+++ .config/nvim/lua/plugins/zenbones.lua
@@ -15,7 +15,7 @@ local specs = lush.parse(function()
     CursorLine   { bg = "#f0f5fa" },
     CursorLineNr { fg = "#607D8B", bg="#f0f5fa" },
     String  { fg = "#33691E", gui = "italic" },
-    Comment { fg = "#8D6E63", gui = "bold,italic" },
+    Comment { fg = "#A1887F", gui = "bold,italic" },
     LineNr  { fg = "#CFD8DC", gui = "bold,italic" },
     Indent1 { fg = "#FFEBEE", gui = "italic" },
     Indent2 { fg = "#E8F5E9", gui = "italic" },
@@ -30,6 +30,15 @@ local specs = lush.parse(function()
     TelescopeSelection { fg = "#FFFFFF", bg = "#1976D2" },
     DiagnosticVirtualTextError { fg = "#D50000", bg = "#FFEBEE" },
     DiagnosticUnderlineError { fg = "#DD2C00", gui = "undercurl" },
+    MarkSignHL { fg = "#009688", bg = "#E0F7FA" },
+    MarkSignNumHL { fg = "#B2DFDB", bg = "#E0F7FA" },
+    GitSignsAdd { fg = "#81C784" },
+    GitSignsAddNr { fg = "#C8E6C9" },
+    GitSignsDelete { fg = "#E53935" },
+    GitSignsDeleteNr { fg = "#FFCDD2" },
+    GitSignsChange { fg = "#FFA726" },
+    GitSignsChangeNr { fg = "#FFE0B2" },
+    goSameId { fg = "#ff0000", gui = "underline" },
   }
 end)
 
