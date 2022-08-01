75d1367a7318c28381c294d50913400ea175aafe
Daniel Lundin <daniel@arity.se>
2022-08-01 15:21:32 +0200

    nvim: add cue fmt to null-ls

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -4,11 +4,13 @@ local builtins = require("null-ls.builtins")
 null_ls.setup({
 	sources = {
 		builtins.formatting.buf,
+		builtins.formatting.cue_fmt,
 		builtins.formatting.shfmt,
 		builtins.formatting.buildifier,
 		builtins.completion.spell,
 		builtins.diagnostics.buf,
 		builtins.diagnostics.buildifier,
+		builtins.diagnostics.cue_fmt,
 	},
 	debug = true,
 })
