0169bf12432e4cb14c71812f474053144eb157a7
Daniel Lundin <daniel@arity.se>
2022-09-10 13:00:48 +0200

    nvim: lualine separators

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -8,7 +8,7 @@ lualine.setup({
 	options = {
 		globalstatus = true,
 		theme = "onelight",
-		component_separators = "╱",
+		component_separators = { left = "╲", right = "╱" },
 		section_separators = { left = "", right = "" },
 	},
 	sections = {
