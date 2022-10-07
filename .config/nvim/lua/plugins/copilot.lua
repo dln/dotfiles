e7dcd1d55bc63c949eeafa3e49b6ffd980db70bf
Daniel Lundin <daniel@arity.se>
2022-10-07 10:29:23 +0200

    nvim: copilot accept fix

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,5 +1,7 @@
-vim.keymap.set("i", "<C-J>", [[copilot#Accept("\<CR>")]], { noremap = true, silent = true, expr = true, script = true })
+vim.keymap.set("i", "<C-J>", [[copilot#Accept("<CR>")]], { noremap = true, silent = true, expr = true, script = true })
 
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
 vim.g.copilot_tab_fallback = ""
+
+vim.g.copilot_no_tab_map = true
