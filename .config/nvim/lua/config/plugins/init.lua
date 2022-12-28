71fb14c028db198b539e9217c92747295738c13f
Daniel Lundin <daniel@arity.se>
2022-12-28 13:32:15 +0100

    nvim: move indent-blankline config to file

--- .config/nvim/lua/config/plugins/init.lua
+++ .config/nvim/lua/config/plugins/init.lua
@@ -39,26 +39,6 @@ return {
 	},
 
 	{
-		"lukas-reineke/indent-blankline.nvim",
-		branch = "master",
-		event = "VeryLazy",
-		init = function()
-			vim.g.indent_blankline_char = "│"
-			vim.g.indent_blankline_space_char = "⬝"
-			vim.g.indent_blankline_space_char_highlight_list = { "IndentSpace" }
-			-- vim.g.indent_blankline_char_list = {'|', '¦', '┆', '┊'}
-			vim.g.indent_blankline_buftype_exclude = { "help", "terminal" }
-			vim.g.indent_blankline_filetype_exclude = { "text", "markdown" }
-			-- vim.g.indent_blankline_show_end_of_line = true
-			vim.g.indent_blankline_show_first_indent_level = true
-			vim.g.indent_blankline_show_trailing_blankline_indent = true
-			vim.g.indent_blankline_char_highlight_list =
-				{ "Indent1", "Indent2", "Indent3", "Indent4", "Indent5", "Indent6" }
-			require("indent_blankline").setup({})
-		end,
-	},
-
-	{
 		"ray-x/go.nvim",
 		config = function()
 			require("go").setup({
