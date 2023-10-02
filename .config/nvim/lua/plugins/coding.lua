b4c284f01ea40251dfb89113363a65823fd13bd1
Daniel Lundin <daniel@arity.se>
2023-10-02 21:51:48 +0200

    nvim: fix mini.comment keymap preference

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -1,14 +1,4 @@
 return {
-
-	{
-		"echasnovski/mini.comment",
-		keys = {
-			{ "<Leader><C-_>", "gcgc", remap = true, silent = true, mode = "n", desc = "Uncomment whole comment" },
-			{ "<C-_>", "gcc", remap = true, silent = true, mode = "n" },
-			{ "<C-_>", "gc", remap = true, silent = true, mode = "v" },
-		},
-	},
-
 	{
 		"echasnovski/mini.pairs",
 		enabled = false,
