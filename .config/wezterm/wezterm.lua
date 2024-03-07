6516c5306f84353ab542c9e5f4a13d37c750ffbf
Daniel Lundin <dln@arity.se>
2024-03-07 19:45:46 +0100

    wezterm: use iosevka shelman font

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,11 +5,11 @@ local act = wezterm.action
 local dev_host = "dln-dev"
 local spawn_dev_nvim = { "ssh", dev_host, "nvim", "--listen", os.getenv("XDG_RUNTIME_DIR") .. "/nvim-persistent.sock" }
 
-local function font_with_fallback(name, params)
-	local names = { name, "Noto Color Emoji" }
-	return wezterm.font_with_fallback(names, params)
-end
-
+-- local function font_with_fallback(name, params)
+-- 	local names = { name, "Noto Color Emoji" }
+-- 	return wezterm.font_with_fallback(names, params)
+-- end
+--
 wezterm.add_to_config_reload_watch_list(os.getenv("HOME") .. "/.config/shelman-theme/current/wezterm")
 
 return {
@@ -17,85 +17,28 @@ return {
 	color_scheme_dirs = {
 		os.getenv("HOME") .. "/.config/shelman-theme/current/wezterm",
 	},
-	font = font_with_fallback("Iosevka Term SS09", { weight = "Light" }),
+	font = wezterm.font({ family = "IosevkaShelman Nerd Font" }),
 	font_rules = {
 		{
 			italic = false,
 			intensity = "Half",
 			reverse = false,
-			font = font_with_fallback("Iosevka Term SS09", { weight = "Thin" }),
-		},
-		{
-			italic = false,
-			intensity = "Bold",
-			reverse = false,
-			font = font_with_fallback("Iosevka Term SS09", { weight = "DemiBold" }),
+			font = wezterm.font("IosevkaShelman Nerd Font", { weight = "ExtraLight" }),
 		},
 		{
 			italic = true,
 			intensity = "Normal",
 			reverse = false,
-			font = font_with_fallback("Iosevka Term Curly Slab", { weight = "Light", italic = true }),
-		},
-		{
-			italic = true,
-			intensity = "Half",
-			reverse = false,
-			font = font_with_fallback("Iosevka Term SS15", { weight = "ExtraLight", italic = true }),
-		},
-		{
-			italic = true,
-			intensity = "Bold",
-			reverse = false,
-			font = font_with_fallback("Iosevka Term Curly Slab Ex", { weight = "Regular", italic = true }),
-		},
-
-		-- Reversed
-		{
-			reverse = true,
-			italic = false,
-			intensity = "Normal",
-			font = font_with_fallback("Iosevka Term SS09", { weight = "Regular" }),
-		},
-		{
-			reverse = true,
-			italic = false,
-			intensity = "Half",
-			font = font_with_fallback("Iosevka Term SS09", { weight = "Light" }),
-		},
-		{
-			reverse = true,
-			italic = true,
-			intensity = "Half",
-			font = font_with_fallback("Iosevka Term Curly Slab", { weight = "ExtraLight", italic = true }),
-		},
-		{
-			reverse = true,
-			italic = true,
-			intensity = "Normal",
-			font = font_with_fallback("Iosevka Term Curly Slab", { weight = "Light", italic = true }),
-		},
-		{
-			reverse = true,
-			italic = true,
-			intensity = "Bold",
-			font = font_with_fallback("Iosevka Term Curly Slab Ex", { weight = "Bold", italic = true }),
-		},
-		{
-			reverse = true,
-			italic = false,
-			intensity = "Bold",
-			font = font_with_fallback("Iosevka Term SS09", { weight = "Bold" }),
+			font = wezterm.font("IosevkaShelman Nerd Font", { weight = "ExtraLight", italic = true }),
 		},
 	},
 	front_end = "WebGpu",
 	webgpu_power_preference = "HighPerformance",
 	warn_about_missing_glyphs = false,
 	bold_brightens_ansi_colors = false,
-	allow_square_glyphs_to_overflow_width = "Always",
-	font_size = 20,
+	-- allow_square_glyphs_to_overflow_width = "Always",
+	font_size = 16,
 	command_palette_font_size = 13.5,
-	line_height = 1.1,
 	initial_cols = 132,
 	initial_rows = 45,
 	use_resize_increments = true,
@@ -107,10 +50,13 @@ return {
 		top = 0,
 		bottom = 0,
 	},
+	unicode_version = 14,
 	default_cursor_style = "SteadyBlock",
 	cursor_thickness = "6px",
 	cursor_blink_rate = 0,
 	hide_mouse_cursor_when_typing = false,
+	underline_position = -12,
+	underline_thickness = 2,
 	enable_wayland = true,
 	enable_tab_bar = false,
 	tab_bar_at_bottom = true,
