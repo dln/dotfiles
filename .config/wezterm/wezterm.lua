fe11201cdb123342caaa3c7c0f20721ca1d47e00
Daniel Lundin <dln@arity.se>
2024-03-11 13:00:26 +0100

    wezterm: new config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,11 +5,6 @@ local act = wezterm.action
 local dev_host = "dln-dev"
 local spawn_dev_nvim = { "ssh", dev_host, "nvim", "--listen", os.getenv("XDG_RUNTIME_DIR") .. "/nvim-persistent.sock" }
 
--- local function font_with_fallback(name, params)
--- 	local names = { name, "Noto Color Emoji" }
--- 	return wezterm.font_with_fallback(names, params)
--- end
---
 wezterm.add_to_config_reload_watch_list(os.getenv("HOME") .. "/.config/shelman-theme/current/wezterm")
 
 return {
@@ -17,46 +12,51 @@ return {
 	color_scheme_dirs = {
 		os.getenv("HOME") .. "/.config/shelman-theme/current/wezterm",
 	},
-	font = wezterm.font({ family = "IosevkaShelman Nerd Font" }),
+	font = wezterm.font({
+		family = "IosevkaShelman Nerd Font",
+		weight = "Light",
+	}),
 	font_rules = {
 		{
-			italic = false,
-			intensity = "Half",
+			italic = true,
+			intensity = "Bold",
 			reverse = false,
-			font = wezterm.font("IosevkaShelman Nerd Font", { weight = "ExtraLight" }),
+			font = wezterm.font("IosevkaShelman Nerd Font", { weight = "ExtraLight", italic = true }),
+			-- font = wezterm.font("Iosevka Term Curly Slab", { weight = "Thin", italic = true }),
 		},
 		{
 			italic = true,
 			intensity = "Normal",
 			reverse = false,
-			font = wezterm.font("IosevkaShelman Nerd Font", { weight = "ExtraLight", italic = true }),
+			font = wezterm.font("IosevkaShelman Nerd Font", { weight = "Light", italic = true }),
 		},
 	},
 	front_end = "WebGpu",
 	webgpu_power_preference = "HighPerformance",
 	warn_about_missing_glyphs = false,
 	bold_brightens_ansi_colors = false,
-	-- allow_square_glyphs_to_overflow_width = "Always",
+	-- allow_square_glyphs_to_overflow_width = "Never",
 	font_size = 16,
 	command_palette_font_size = 13.5,
+	line_height = 1.0,
 	initial_cols = 132,
 	initial_rows = 45,
 	use_resize_increments = true,
 	adjust_window_size_when_changing_font_size = false,
 	window_decorations = "RESIZE",
 	window_padding = {
-		left = 0,
-		right = 0,
+		left = 6,
+		right = 6,
 		top = 0,
 		bottom = 0,
 	},
 	unicode_version = 14,
 	default_cursor_style = "SteadyBlock",
 	cursor_thickness = "6px",
-	cursor_blink_rate = 0,
+	cursor_blink_rate = 700,
 	hide_mouse_cursor_when_typing = false,
-	underline_position = -12,
-	underline_thickness = 2,
+	underline_position = -9,
+	underline_thickness = 1,
 	enable_wayland = true,
 	enable_tab_bar = false,
 	tab_bar_at_bottom = true,
@@ -77,10 +77,6 @@ return {
 		{ key = "-", mods = "CTRL", action = "DecreaseFontSize" },
 		{ key = "=", mods = "CTRL", action = "IncreaseFontSize" },
 		{ key = "Enter", mods = "ALT", action = "ToggleFullScreen" },
-		{ key = "r", mods = "ALT", action = act.ReloadConfiguration },
-		{ key = "L", mods = "CTRL", action = wezterm.action.ShowDebugOverlay },
-		{ key = "l", mods = "ALT", action = wezterm.action.ActivateCommandPalette },
-		{ key = "Backspace", mods = "ALT", action = act.SwitchWorkspaceRelative(1) },
 		{ key = "UpArrow", mods = "SHIFT", action = act.ScrollByLine(-1) },
 		{ key = "DownArrow", mods = "SHIFT", action = act.ScrollByLine(1) },
 		{ key = "PageUp", mods = "SHIFT", action = act.ScrollByPage(-0.5) },
