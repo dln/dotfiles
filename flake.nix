01b97bf22c53658cf8965609b49d6c22b762f9bc
Daniel Lundin <dln@arity.se>
2024-11-07 01:20:00 +0100

    nix: add cachix community cache

--- flake.nix
+++ flake.nix
@@ -1,6 +1,18 @@
 {
   description = "NixOS configuration";
 
+  nixConfig = {
+    substituters = [
+      "https://cache.nixos.org/"
+    ];
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
