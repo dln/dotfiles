f53cbba16fb02334d1842185884ec5a6fde0f91b
Daniel Lundin <dln@arity.se>
2024-12-11 20:15:15 +0100

    Enable avahi

--- common/base.nix
+++ common/base.nix
@@ -51,6 +51,18 @@
     '';
   };
 
+  services.avahi = {
+    nssmdns4 = true;
+    enable = true;
+    ipv4 = true;
+    ipv6 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+    };
+  };
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
