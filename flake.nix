7152b0bf09f56410ad4f39e22ef5f717322b370c
Daniel Lundin <dln@arity.se>
2025-01-04 13:00:29 +0100

    nix: add nix-index

--- flake.nix
+++ flake.nix
@@ -21,6 +21,9 @@
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
+    nix-index-database.url = "github:nix-community/nix-index-database";
+    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
@@ -28,6 +31,7 @@
   outputs =
     inputs@{
       self,
+      nix-index-database,
       nixpkgs,
       home-manager,
       ...
@@ -55,6 +59,7 @@
           };
           modules = [
             ./home/common
+            nix-index-database.hmModules.nix-index
           ] ++ modules;
         };
     in
