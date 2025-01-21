04fea5639d917b8c788c657012f07e2710d4b15a
Daniel Lundin <dln@arity.se>
2025-01-21 10:29:47 +0100

    ghostty: themes are supported in home-manager now

--- home/common/ghostty.nix
+++ home/common/ghostty.nix
@@ -47,59 +47,59 @@
           "super+enter=toggle_fullscreen"
         ];
       };
+      themes = {
+        PatagiaDark = {
+          background = if config.patagia.oled.enable then "#000000" else "#14151a";
+          foreground = "#b7bec7";
+          cursor-color = "#e7e7b7";
+          selection-background = "#84979f";
+          selection-foreground = "#000000";
+          palette = [
+            "#000000"
+            "#ff0035"
+            "#85ff00"
+            "#ffc900"
+            "#00a7ff"
+            "#cb01ff"
+            "#00e0ff"
+            "#b7bec7"
+            "#444444"
+            "#ff8c88"
+            "#baff94"
+            "#ffe090"
+            "#88ccff"
+            "#e38dff"
+            "#97eeff"
+            "#ffffff"
+          ];
+        };
+        PatagiaLight = {
+          background = "#fefeff";
+          foreground = "#222222";
+          cursor-color = "#aa0000";
+          selection-background = "#ffe6a4";
+          selection-foreground = "#483600";
+          palette = [
+            "#000000"
+            "#9e001d"
+            "#306300"
+            "#deae00"
+            "#00669e"
+            "#7d009e"
+            "#008a9e"
+            "#f7f7f7"
+            "#b0b0b0"
+            "#ff0035"
+            "#509e00"
+            "#ffc900"
+            "#00a7ff"
+            "#cb01ff"
+            "#00e0ff"
+            "#ffffff"
+          ];
+        };
+      };
     };
-
-    xdg.configFile."ghostty/themes/PatagiaDark".text =
-      let
-        background = if config.patagia.oled.enable then "#000000" else "#14151a";
-      in
-      ''
-        background = "${background}"
-        foreground = #b7bec7
-        cursor-color = #e7e7b7
-        selection-background = #84979f
-        selection-foreground = #000000
-        palette = 0=#000000
-        palette = 1=#ff0035
-        palette = 2=#85ff00
-        palette = 3=#ffc900
-        palette = 4=#00a7ff
-        palette = 5=#cb01ff
-        palette = 6=#00e0ff
-        palette = 7=#b7bec7
-        palette = 8=#444444
-        palette = 9=#ff8c88
-        palette = 10=#baff94
-        palette = 11=#ffe090
-        palette = 12=#88ccff
-        palette = 13=#e38dff
-        palette = 14=#97eeff
-        palette = 15=#ffffff
-      '';
-
-    xdg.configFile."ghostty/themes/PatagiaLight".text = ''
-      background = #fefeff
-      foreground = #222222
-      cursor-color = #aa0000
-      selection-background = #ffe6a4
-      selection-foreground = #483600
-      palette = 0=#000000
-      palette = 1=#9e001d
-      palette = 2=#306300
-      palette = 3=#deae00
-      palette = 4=#00669e
-      palette = 5=#7d009e
-      palette = 6=#008a9e
-      palette = 7=#f7f7f7
-      palette = 8=#b0b0b0
-      palette = 9=#ff0035
-      palette = 10=#509e00
-      palette = 11=#ffc900
-      palette = 12=#00a7ff
-      palette = 13=#cb01ff
-      palette = 14=#00e0ff
-      palette = 15=#ffffff
-    '';
 
     xdg.desktopEntries = {
       ghostty-secondary = {
