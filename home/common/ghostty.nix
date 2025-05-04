ee7e6f767de8cf879fcf8abe0a5c6f1a626092d4
Daniel Lundin <dln@arity.se>
2025-05-04 07:34:28 +0200

    ghostty: devel desktop links only on laptop

--- home/common/ghostty.nix
+++ home/common/ghostty.nix
@@ -87,44 +87,6 @@
         terminal = false;
         type = "Application";
       };
-
-      ghostty-devel = {
-        categories = [
-          "System"
-          "TerminalEmulator"
-        ];
-        exec = ''
-          ghostty --class=com.mitchellh.ghostty-devel --command="ssh -t devel" --initial-command="ssh -t devel"
-        '';
-        genericName = "Ghostty (devel)";
-        icon = "com.mitchellh.ghostty";
-        name = "Ghostty (devel)";
-        settings = {
-          StartupWMClass = "com.mitchellh.ghostty-devel";
-          TryExec = "ghostty";
-        };
-        terminal = false;
-        type = "Application";
-      };
-
-      ghostty-devel-secondary = {
-        categories = [
-          "System"
-          "TerminalEmulator"
-        ];
-        exec = ''
-          ghostty --class=com.mitchellh.ghostty-devel-secondary --background-opacity=0.8 --font-style="ExtraCondensed" --font-style-bold="Bold ExtraCondensed" --font-style-italic="ExtraCondensed Oblique" --command="ssh -t devel" --initial-command="ssh -t devel"
-        '';
-        genericName = "Secondary Ghostty (devel)";
-        icon = "com.mitchellh.ghostty";
-        name = "Secondary Ghostty (devel)";
-        settings = {
-          StartupWMClass = "com.mitchellh.ghostty-devel-secondary";
-          TryExec = "ghostty";
-        };
-        terminal = false;
-        type = "Application";
-      };
     };
 
   };
