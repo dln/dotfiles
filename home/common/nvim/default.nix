a3de1f893e829dc862acb17a6de3f530603f367b
Daniel Lundin <dln@arity.se>
2025-06-26 19:12:50 +0200

    jj: Add diffconflicts merge tool

--- home/common/nvim/default.nix
+++ home/common/nvim/default.nix
@@ -101,6 +101,19 @@ in
 
       {
         plugin = pkgs.vimUtils.buildVimPlugin {
+          name = "jj-diffconflicts";
+          src = pkgs.fetchFromGitHub {
+            owner = "rafikdraoui";
+            repo = "jj-diffconflicts";
+            rev = "8140e5295ef2008a947f1f374c2d71a5bc7e38a0";
+            hash = "sha256-LM2eP29yK+lIlWzJiIKIRcbVjNhyjV2unE4GJDTLKXQ=";
+          };
+        };
+        type = "lua";
+      }
+
+      {
+        plugin = pkgs.vimUtils.buildVimPlugin {
           name = "dieter-nvim";
           src = ./dieter;
         };
--- home/dln/home.nix
+++ home/dln/home.nix
@@ -69,6 +69,20 @@ in
       };
 
       "merge-tools" = {
+        diffconflicts = {
+          program = "nvim";
+          merge-args = [
+            "-c"
+            "let g:jj_diffconflicts_marker_length=$marker_length"
+            "-c"
+            "JJDiffConflicts!"
+            "$output"
+            "$base"
+            "$left"
+            "$right"
+          ];
+          merge-tool-edits-conflict-markers = true;
+        };
         difft."diff-args" = [
           "--color=always"
           "$left"
