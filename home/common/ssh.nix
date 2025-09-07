85289147ca6e03f85c504021efbbe7a8c4b391d0
Daniel Lundin <dln@arity.se>
2025-09-07 15:29:16 +0200

    ssh: config refactor i nix

--- home/common/ssh.nix
+++ home/common/ssh.nix
@@ -2,13 +2,21 @@
 {
   programs.ssh = {
     enable = true;
-    addKeysToAgent = "yes";
-    compression = false;
-    controlMaster = "auto";
-    controlPath = "\${XDG_RUNTIME_DIR}/ssh_control:%h:%p:%r";
-    controlPersist = "15m";
+    enableDefaultConfig = false;
     extraConfig = ''
       PreferredAuthentications publickey
     '';
+    matchBlocks."*" = {
+      addKeysToAgent = "yes";
+      compression = false;
+      controlMaster = "auto";
+      controlPath = "\${XDG_RUNTIME_DIR}/ssh_control:%h:%p:%r";
+      controlPersist = "15m";
+      forwardAgent = false;
+      hashKnownHosts = false;
+      serverAliveCountMax = 3;
+      serverAliveInterval = 0;
+      userKnownHostsFile = "~/.ssh/known_hosts";
+    };
   };
 }
