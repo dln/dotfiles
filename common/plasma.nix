dd3c2c756c5ad15e140d9e6055e8b789d9d76728
Daniel Lundin <dln@arity.se>
2025-09-27 15:07:25 +0200

    plasma: add apps

--- common/plasma.nix
+++ common/plasma.nix
@@ -13,17 +13,29 @@
   };
 
   environment.systemPackages = with pkgs; [
+    haruna
     kdePackages.discover
     kdePackages.isoimagewriter
     kdePackages.kcalc
     kdePackages.kcharselect
     kdePackages.kclock
     kdePackages.kcolorchooser
+    kdePackages.kdenlive
     kdePackages.partitionmanager
     kdePackages.polkit-kde-agent-1
     kdePackages.sddm-kcm
     kdiff3
   ];
+
+  programs.kdeconnect.enable = true;
+  networking.firewall = {
+    allowedTCPPortRanges = [
+      { from = 1714; to = 1764; }
+    ];
+    allowedUDPPortRanges = [
+      { from = 1714; to = 1764; }
+    ];
+  };
 
   programs.ssh.askPassword = lib.getExe pkgs.kdePackages.ksshaskpass;
 }
