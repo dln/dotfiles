edd87a1604567124db5698c7618ff4117a8c00fa
Daniel Lundin <dln@arity.se>
2025-10-08 10:34:52 +0200

    nemo: music apps

--- home/dln/nemo.nix
+++ home/dln/nemo.nix
@@ -19,7 +19,21 @@
     ];
   };
 
+  programs.beets = {
+    enable = true;
+  };
+
+  home.sessionVariables = {
+    WRTAG_ADDON = "musicdesc,replaygain";
+    WRTAG_PATH_FORMAT = ''/home/dln/Music/Library/{{ artists .Release.Artists | sort | join \": \" | safepath }}/{{ .Release.Title | safepath }}{{ if not (eq .ReleaseDisambiguation \"\") }} ({{ .ReleaseDisambiguation | safepath }}){{ end }} ({{ .Release.ReleaseGroup.FirstReleaseDate.Year }})/{{ pad0 2 .TrackNum }}.{{ len .Tracks | pad0 2 }} {{ if .IsCompilation }}{{ artistsString .Track.Artists | safepath }} - {{ end }}{{ .Track.Title | safepath }}{{ .Ext }}'';
+  };
+
   home.packages = with pkgs; [
     stable.calibre
+    beets
+    essentia-extractor
+    picard
+    rsgain
+    wrtag
   ];
 }
