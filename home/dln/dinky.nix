9d3d0730951bae26a0157d04122189195972fd0d
Daniel Lundin <dln@arity.se>
2025-10-25 21:31:22 +0200

    dinky: add obs

--- home/dln/dinky.nix
+++ home/dln/dinky.nix
@@ -19,4 +19,13 @@
     stable.calibre
     wrtag
   ];
+
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs; [
+      obs-studio-plugins.wlrobs
+      obs-studio-plugins.obs-vaapi
+      obs-studio-plugins.obs-pipewire-audio-capture
+    ];
+  };
 }
