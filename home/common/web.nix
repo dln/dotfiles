8390011111c1000a86f5de77106bfa2f620e20aa
Daniel Lundin <dln@arity.se>
2025-11-14 12:22:35 +0100

    chromium: extensions and args

--- home/common/web.nix
+++ home/common/web.nix
@@ -16,6 +16,45 @@
     programs.chromium = {
       enable = true;
       package = pkgs.ungoogled-chromium;
+      extensions = [
+        "ddkjiahejlhfcafbddmgiahcphecmpfh" # uBlock Origin Lite
+      ];
+
+      commandLineArgs = [
+        # Wayland
+        "--ozone-platform=wayland"
+        "--ozone-platform-hint=auto"
+        # Performance
+        "--gtk-version=4"
+        "--ignore-gpu-blocklist"
+        "--enable-gpu-rasterization"
+        "--enable-oop-rasterization"
+        "--enable-zero-copy"
+        # Etc
+        "--disk-cache=$XDG_RUNTIME_DIR/chromium-cache"
+        "--disable-reading-from-canvas"
+        "--no-first-run"
+        "--disable-wake-on-wifi"
+        "--disable-speech-api"
+        "--disable-speech-synthesis-api"
+        # Use strict extension verification
+        "--extension-content-verification=enforce_strict"
+        "--extensions-install-verification=enforce_strict"
+        # Disable pings
+        "--no-pings"
+        # Require HTTPS for component updater
+        "--component-updater=require_encryption"
+        # Disable crash upload
+        "--no-crash-upload"
+        # don't run things without asking
+        "--no-service-autorun"
+        # Disable sync
+        "--disable-sync"
+        # Disable autofill
+        "AutofillPaymentCardBenefits"
+        "AutofillPaymentCvcStorage"
+        "AutofillPaymentCardBenefits"
+      ];
     };
 
   };
