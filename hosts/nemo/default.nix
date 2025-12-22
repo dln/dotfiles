57fcab605db0ba2e6a9761d055847980a636d985
Daniel Lundin <dln@arity.se>
2025-12-22 22:56:13 +0100

    nemo: add kernel patch for dp->hdmi adapter

--- hosts/nemo/default.nix
+++ hosts/nemo/default.nix
@@ -51,6 +51,18 @@
       "amdgpu.si_support=1"
       "amdgpu.cik_support=1"
     ];
+    kernelPatches = [
+      {
+        # See: https://gitlab.freedesktop.org/drm/amd/-/issues/4773
+        name = "amdgpu-fix-issue-4773";
+        patch = ./0001-amdgpu-Add-CH7218-PCON-to-the-VRR-whitelist.patch;
+      }
+      {
+        # See: https://gitlab.freedesktop.org/drm/amd/-/issues/4773
+        name = "amdgpu-hdmi-vrr";
+        patch = ./hdmi-vrr.patch;
+      }
+    ];
   };
 
   fileSystems."/" = {
